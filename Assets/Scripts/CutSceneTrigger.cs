17faf1478763bcfeae76557f0c9867caac6cafbf
snowWolf350 <pranet.v08@gmail.com>
2026-03-07 23:56:38 +0530

    added cutscene

---
+++ Assets/Scripts/CutSceneTrigger.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class CutSceneTrigger : MonoBehaviour
+{
+    public bool _OnCollisionTrigger;
+
+    BoxCollider _boxCollider;
+    PlayableDirector _playableDirector;
+    [SerializeField] GameObject _timelineGameObject;
+
+    private void Start()
+    {
+        _boxCollider = GetComponent<BoxCollider>();
+        _playableDirector = _timelineGameObject.GetComponent<PlayableDirector>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.GetComponent<ThirdPersonShooterController>() == null) return;
+        
+        //plaayer hit the collider 
+        if (_OnCollisionTrigger)
+        {
+            StartCoroutine(PlayCutscene());
+        }
+    }
+
+    IEnumerator PlayCutscene()
+    {
+        _playableDirector.Play();
+        _boxCollider.enabled = false;
+        yield return new WaitForSeconds((float)_playableDirector.duration);
+        Destroy(_playableDirector.gameObject);
+        Destroy(gameObject);
+    }
+
+}
